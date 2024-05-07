dda2a795fa1055d7c9d392ccfa09d03e66fcd3f3
Mahnoor kibrea <160492529+mahnoor-k29@users.noreply.github.com>
2024-05-07 14:04:34 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,34 @@
+#! /usr/bin/env/ node
+
+
+// this upper line is known as shabang 
+import inquirer from"inquirer";
+let answers=await inquirer.prompt([
+    {message: "Enter First Number" , type:"number", name: "firstNumber"},
+
+    {message:" Enter Second Number", type:"number", name:"secondNumber"},
+    {
+        message:"Select one Operator to perform Operations",
+        type:"list",
+        name:"operator",
+        choices:["Addition", "Subtraction", "Multiplication", "Division"],
+    }
+]);
+console.log(answers);
+
+if(answers.operator=== "Addition"){
+console.log(answers.firstNumber + answers.secondNumber)
+}
+else if (answers.operator=== "Subtraction"){
+console.log(answers.firstNumber - answers.secondNumber)
+}
+else if (answers.operator==="Multiplication"){
+    console.log(answers.firstNumber * answers.secondNumber)
+}
+else if (answers.operator==="Division"){
+    console.log(answers.firstNumber / answers.secondNumber)
+}
+else{
+    console.log("Invalid Input")
+}
+// 25 min
